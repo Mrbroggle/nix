f5e6780be55c9c30a0835f1d7b58c6b6fe9de436
Grady Brown <gb2282006@gmail.com>
2025-02-04 20:27:02 +1100

    fixed security cause i dislike passwords

--- modules/security.nix
+++ modules/security.nix
@@ -1,23 +1,37 @@
 {...}: {
-  security.doas.enable = true;
-  security.doas.extraRules = [
-    {
-      users = ["gradyb"];
+  security = {
+    doas.enable = true;
+    doas.extraRules = [
+      {
+        users = ["gradyb"];
 
-      keepEnv = true;
-      noPass = true;
-    }
-  ];
-
+        keepEnv = true;
+        noPass = true;
+      }
+    ];
+    sudo.extraRules = [
+      {
+        users = ["gradyb"];
+        commands = [
+          {
+            command = "ALL";
+            options = ["SETENV" "NOPASSWD"];
+          }
+        ];
+      }
+    ];
+  };
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
   };
-  services.openssh.enable = true;
 
   systemd.services.fprintd = {
     wantedBy = ["multi-user.target"];
     serviceConfig.Type = "simple";
   };
-  services.fprintd.enable = true;
+  services = {
+    fprintd.enable = true;
+    openssh.enable = true;
+  };
 }
