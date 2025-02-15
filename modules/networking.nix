566748d7b8bf50ae8a2bff2b3f9e111540dde7a0
Grady Brown <gb2282006@gmail.com>
2025-02-15 19:18:31 +1100

    added tailscale

--- modules/networking.nix
+++ modules/networking.nix
@@ -3,4 +3,8 @@
   # networking.wireless.iwd.enable = true;
   networking.networkmanager.enable = true;
   # networking.networkmanager.wifi.backend = "iwd";
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "both";
+  };
 }
