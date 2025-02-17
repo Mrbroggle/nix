e2158bd82788f3f3e5bbb5ba88f3e0b50e9d0869
Grady Brown <gb2282006@gmail.com>
2025-02-17 14:31:50 +1100

    dolphinrc to fix stylix

---
+++ home/dots.nix
@@ -0,0 +1,21 @@
+{...}: {
+  xdg.configFile."dolphinrc".text = ''
+    [General]
+    Version=202
+    ViewPropsTimestamp=2025,2,1,12,28,14.355
+
+    [KFileDialog Settings]
+    Places Icons Auto-resize=false
+    Places Icons Static Size=22
+
+    [MainWindow]
+    MenuBar=Disabled
+    ToolBarsMovable=Disabled
+
+    [UiSettings]
+    ColorScheme=*
+
+    [ViewPropertiesDialog]
+    1536x1024 screen: Window-Maximized=true
+  '';
+}
