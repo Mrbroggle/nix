69bf17d90268b8307b7d60bc0a9674bd6238b2d4
gradyb <gradyb@gradyb.info>
2025-04-13 11:33:59 +1000

    fixed desktop files missing

--- modules/desktopDefault.nix
+++ modules/desktopDefault.nix
@@ -1,10 +1,12 @@
 {...}: {
   imports = [
+    ./browser.nix
     ./desktopApps.nix
     ./office.nix
     ./plymouth.nix
     ./kanata
     ./multirotor.nix
+    ./mail.nix
   ];
 
   boot.loader.systemd-boot.enable = true;
